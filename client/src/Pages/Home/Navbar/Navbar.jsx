c566bd69f4c1408282062a4effe60a2a1fbdc4ce
SteveTambo <stevetambo32@gmail.com>
2026-04-11 14:16:19 +0300

    solana integration

--- client/src/Pages/Home/Navbar/Navbar.jsx
+++ client/src/Pages/Home/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Connection, PublicKey } from "@solana/web3.js";
 
 import "./navbar.css";
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [hbxBalance, setHbxBalance] = useState(null);
+
+  const HBX_MINT_ADDRESS = "HUYpjVt3CLaLzU7vonS4mRCP3RQ312c6SigjdESHpaJP";
 
   const toggleNav = () => {
     setNavActive(!navActive);
@@ -14,6 +19,7 @@ function Navbar() {
     setNavActive(false);
   };
 
+  //Use effect blocks
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 500) {
@@ -34,10 +40,72 @@ function Navbar() {
     }
   }, []);
 
+  // wallet connection and balance fetching
+  const connectWallet = async () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+
+      if (provider.isPhantom) {
+        try {
+          const response = await provider.connect();
+
+          setWalletAddress(response.publicKey.toString());
+
+          getHBXBalance(response.publicKey);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    } else {
+      alert("Phantom wallet not detected");
+    }
+  };
+
+  const getHBXBalance = async (publicKey) => {
+    try {
+      const connection = new Connection(
+        "https://mainnet.helius-rpc.com/?api-key=aa25d1c1-f1d3-4fc4-b7ee-09f655f395e3",
+      );
+
+      const accounts = await connection.getParsedTokenAccountsByOwner(
+        publicKey,
+        {
+          mint: new PublicKey(HBX_MINT_ADDRESS),
+        },
+      );
+
+      if (accounts.value.length > 0) {
+        const balance =
+          accounts.value[0].account.data.parsed.info.tokenAmount.uiAmount;
+        console.log("HBX Balance:", balance);
+
+        setHbxBalance(balance);
+      } else {
+        setHbxBalance(0);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // format balance
+
+  function formatBalance(balance) {
+    if (balance === null || balance === undefined) return "...";
+
+    const num = Number(balance);
+
+    if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(2) + "B";
+    if (num >= 1_000_000) return (num / 1_000_000).toFixed(2) + "M";
+    if (num >= 1_000) return (num / 1_000).toFixed(2) + "K";
+
+    return num.toString();
+  }
+
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       {/* <div className="navbar--logo">
-        <img src="/img/hbx_logo.png" alt="HBX logo" />
+        <img src="/img/hbxlogo.png" alt="HBX logo" />
       </div> */}
       <a
         className={`nav__hamburger ${navActive ? "active" : ""}`}
@@ -103,18 +171,25 @@ function Navbar() {
           </li>
         </ul>
       </div>
-      <Link
-        onClick={closeMenu}
-        activeClass="navbar--active-content"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-        to="/contact"
+      <button
+        onClick={connectWallet}
         className="btn btn-outline-primary"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "4px",
+        }}
       >
-        Contact Us
-      </Link>
+        <img
+          src="/img/phantomlogo.png"
+          alt="phantom"
+          style={{ width: "44px", height: "44px", paddingBottom: "4px" }}
+        />
+
+        {walletAddress
+          ? `$HBX: ${formatBalance(hbxBalance)}`
+          : "Connect Wallet"}
+      </button>
     </nav>
   );
 }
